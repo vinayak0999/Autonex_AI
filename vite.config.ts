dfc4e7ad142855d4097a584c7e54bb0982d82a01
Lavish-code <ylavish07@gmail.com>
2025-10-10 22:03:50 +0530

    Corrected few files

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
+  base: "/",
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
